e7fdaac25f75c749ccd16313055de71ad6371a99
Edilson <edilson_js@yahoo.com.br>
2017-10-16 22:00:02 -0200

    Estrutura Loja

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lojavirtual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
